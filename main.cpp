7fd4a0e18676c9d83b815ba5dcf3e0a4c87b5520
tobam <tobamohaqqeq84@gmail.com>
2025-10-06 04:26:49 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+//Toba Mohaqqeq
+//COMSC-210
+//LAB-19
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,4 +10,37 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+ using namespace std;
 
+    struct Review {
+        double rating;
+        string comment;
+        Review* next;
+    };
+    class Movie {
+        private:
+            string title;
+            Review* head;
+        public:
+            Movie (string t = "Untitled") {
+              title = t;
+              head = nullptr;
+            }
+~Movie() {
+  Review current = head;
+  while (current){
+    Review* temp =current;
+    current = current->next;
+    delete temp;
+  }
+  head = nullptr;
+}
+void addReview(double rating, const string& comment){
+    Review* newReview = new Review;
+    newReview->rating = rating;
+    newReview->comment = comment;
+    newReview->next = head;
+    head = newReview;
+}
+
+    }
